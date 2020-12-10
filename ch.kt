fb4a0b17248f41be47867f1b428eeed9d0c8fb23
Bo Yang <yang.wilby@gmail.com>
2020-12-10 17:01:10 +0100

    add kotlin cheatsheet

---
+++ ch.kt
@@ -0,0 +1,68 @@
+fun start(): String = "OK"
+
+
+## Default and named arguments 
+
+fun joinOptions(options: Collection<String>) =
+        options.joinToString(prefix = "[", postfix = "]")
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+fun getPattern(): String = """\d{2} $month \d{4}"""
+
+
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+fun getPattern(): String = """\d{2} $month \d{4}"""
+
+
+## nullable
+
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) {
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
+}
+
+
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+
+
+## Nothing type
+
+import java.lang.IllegalArgumentException
+
+fun failWithWrongAge(age: Int?): Nothing {
+    throw IllegalArgumentException("Wrong age: $age")
+}
+
+fun checkAge(age: Int?) {
+    if (age == null || age !in 0..150) failWithWrongAge(age)
+    println("Congrats! Next year you'll be ${age + 1}.")
+}
+
+fun main() {
+    checkAge(10)
+}
+
+## lamda
+fun containsEven(collection: Collection<Int>): Boolean =
+        collection.any { it % 2 == 0 }
+
+## data class, equals/hashCode, toString and some others are generated
+
+fun containsEven(collection: Collection<Int>): Boolean =
+        collection.any { it % 2 == 0 }
